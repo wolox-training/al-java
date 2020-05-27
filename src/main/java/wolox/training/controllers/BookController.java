e4fda42642967cd38515f843cbf5fff7d3b765dc
adlamas <alan12394@hotmail.com>
2020-05-27 16:14:41 -0300

    created controller and view

--- src/main/java/wolox/training/controllers/BookController.java
+++ src/main/java/wolox/training/controllers/BookController.java
@@ -1,8 +1,17 @@
 package wolox.training.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class BookController {
 
-    public String Greeting(){
-        return "Hola mundo";
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="Carlos", required=false)
+        String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
