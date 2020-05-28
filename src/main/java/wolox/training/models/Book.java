0f5cd4c40f5f2c4f4111f7c8a3ea93a618dd4905
adlamas <alan12394@hotmail.com>
2020-05-28 10:45:46 -0300

    created Book model

---
+++ src/main/java/wolox/training/models/Book.java
@@ -0,0 +1,32 @@
+package wolox.training.models;
+
+public class Book {
+
+    @Column()
+    private String genre;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String subtitle;
+
+    @Column(nullable = false)
+    private String publisher;
+
+    @Column(nullable = false)
+    private String year;
+
+    @Column(nullable = false)
+    private int pages;
+
+    @Column(nullable = false)
+    private String isbn;
+
+}
