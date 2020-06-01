93b37cb0d8ec3d2aaeb91bf6f2bcf566797313f8
adlamas <alan12394@hotmail.com>
2020-06-01 18:25:58 -0300

    created getters and setters in User model

--- src/main/java/wolox/training/models/User.java
+++ src/main/java/wolox/training/models/User.java
@@ -1,6 +1,8 @@
 package wolox.training.models;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -10,11 +12,14 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+import wolox.training.exceptions.BookAlreadyOwnedException;
 
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "USER_SEQ")
+    @SequenceGenerator(name = "USER_SEQ", sequenceName = "USER_SEQ")
     private long id;
 
     @Column(nullable = false)
@@ -30,7 +35,50 @@ public class User {
     @JoinTable(name = "book_user",
         joinColumns = @JoinColumn(name = "book_id", referencedColumnName = "id"),
         inverseJoinColumns = @JoinColumn(name = "author_id", referencedColumnName = "id"))
-    private List<Book> Books;
+    private List<Book> books = new ArrayList<Book>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public List<Book> getBooks() {
+        return (List<Book>) Collections.unmodifiableList(books);
+    }
+
+    public void addBook(Book book) throws BookAlreadyOwnedException {
+        if (books.contains(book)) {
+            throw new BookAlreadyOwnedException();
+        }
+        this.books.add(book);
+    }
 
 
     public User(){}
