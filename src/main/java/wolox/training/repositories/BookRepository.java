3e9d392e0947b856b5d56b9c1d2b04008ecdc6a8
adlamas <alan12394@hotmail.com>
2020-06-02 16:48:41 -0300

    added java doc to both query in repositories

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -3,7 +3,14 @@ package wolox.training.repositories;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import wolox.training.models.Book;
+import wolox.training.models.User;
 
 public interface BookRepository extends JpaRepository<Book,Long> {
+
+    /**
+     *
+     * @param author (Method will filter by Books Author)
+     * @return {@link Optional< Book >} (Will return a Book or void)
+     */
     Optional<Book> findFirstByAuthor(final String author);
 }
--- src/main/java/wolox/training/repositories/UserRepository.java
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -5,5 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import wolox.training.models.User;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    /**
+     *
+     * @param username (Method will filter by username)
+     * @return {@link Optional<User>} (Will return an User or void)
+     */
     Optional<User> findFirstByUsername(final String username);
 }
