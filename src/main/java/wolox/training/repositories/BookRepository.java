15f2cdb71ee1597c3bd3654ed08369eaa993b5f6
adlamas <alan12394@hotmail.com>
2020-06-04 11:27:31 -0300

    added javadoc description in method

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -3,12 +3,11 @@ package wolox.training.repositories;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import wolox.training.models.Book;
-import wolox.training.models.User;
 
 public interface BookRepository extends JpaRepository<Book,Long> {
 
     /**
-     *
+     * Returns the first Book that it finds filtered by its author
      * @param author (Method will filter by Books Author)
      * @return {@link Optional< Book >} (Will return a Book or void)
      */
