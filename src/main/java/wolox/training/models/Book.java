4043523c807309cfc7095aa826f48af94569acd4
adlamas <alan12394@hotmail.com>
2020-06-09 14:49:32 -0300

    fixed typo

--- src/main/java/wolox/training/models/Book.java
+++ src/main/java/wolox/training/models/Book.java
@@ -11,7 +11,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import javax.persistence.SequenceGenerator;Use
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import wolox.training.exceptions.ErrorConstants;
 import org.apache.commons.lang3.StringUtils;
