52160c508839ba48a3db5dd2e39f9a66b59837e2
adlamas <alan12394@hotmail.com>
2020-06-11 14:42:34 -0300

    created test to userById function

--- src/test/java/wolox/training/controllers/UserControllerTest.java
+++ src/test/java/wolox/training/controllers/UserControllerTest.java
@@ -12,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import wolox.training.models.User;
 import wolox.training.repositories.BookRepository;
 import wolox.training.repositories.UserRepository;
@@ -27,7 +29,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
-
 public class UserControllerTest {
 
     @Autowired
@@ -47,7 +48,7 @@ public class UserControllerTest {
     void setUp(){
         javier = new User("JaviMo", "Javier Moreno", LocalDate.parse("1980-10-08"));
         marihe = new User("MariHe", "Maria Haize", LocalDate.parse("1990-11-04"));
-        robertV = new User("Robert Velvet", "Robert Velvet", LocalDate.parse("1975-04-11"));
+        robertV = new User("RobertV", "Robert Velvet", LocalDate.parse("1975-04-11"));
     }
 
     List<User> allUsers = new ArrayList<User>();
@@ -62,5 +63,25 @@ public class UserControllerTest {
             .andExpect(content().contentType("application/json"))
             .andExpect(jsonPath("$", hasSize(1)))
             .andExpect(jsonPath("$[0].username", is(javier.getUsername() )));
+    }
+
+    @Test
+    public void givenUser_whenGetUserById_thenReturnJsonArrayWithCorrectUser() throws Exception {
+        given(mockedUserRepo.findById(marihe.getId())).willReturn(
+            java.util.Optional.ofNullable(marihe));
+
+        mockMvc.perform(get("/api/users/" + marihe.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType("application/json"))
+            .andExpect(jsonPath("$.username", is(marihe.getUsername() )));
+    }
+
+    @Test
+    public void givenUser_whenGetUserByUsername_thenRaiseNotFoundException() throws Exception {
+        given(mockedUserRepo.findById(marihe.getId())).willReturn(
+            java.util.Optional.ofNullable(marihe));
+
+        mockMvc.perform(get("/api/users/" + -1))
+            .andExpect(status().isNotFound());
     }
 }
