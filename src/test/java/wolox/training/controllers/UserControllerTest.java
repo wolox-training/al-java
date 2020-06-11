16954af1f34cc315043d9431139092e2d9b7cd95
adlamas <alan12394@hotmail.com>
2020-06-11 12:20:42 -0300

    updated User and updated test

--- src/test/java/wolox/training/controllers/UserControllerTest.java
+++ src/test/java/wolox/training/controllers/UserControllerTest.java
@@ -1,61 +1,66 @@
 package wolox.training.controllers;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import wolox.training.models.User;
+import wolox.training.repositories.BookRepository;
 import wolox.training.repositories.UserRepository;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
 
 public class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-   /*
 
     @MockBean
-    private UserRepository userRepo;
+    private UserRepository mockedUserRepo;
 
     @MockBean
-    private User alex;
+    private BookRepository mockedBookRepo;
+
+    private User javier;
+    private User marihe;
+    private User robertV;
 
     @BeforeEach
     void setUp(){
-        alex = new User();
+        javier = new User("JaviMo", "Javier Moreno", LocalDate.parse("1980-10-08"));
+        marihe = new User("MariHe", "Maria Haize", LocalDate.parse("1990-11-04"));
+        robertV = new User("Robert Velvet", "Robert Velvet", LocalDate.parse("1975-04-11"));
     }
 
-    @Test
-    public void givenUsers_whenGetUsers_thenReturnJsonArray() throws Exception {
-        List<User> allUser = Arrays.asList(alex);
-        given(userRepo.findAll()).willReturn(allUser);
-
-        //mockMvc.perform(get("/all")).contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
-        mockMvc.perform(get("/all"))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$", hasSize(1)))
-            .andExpect(jsonPath("$[0].name", is(alex.getName() )));
-    }*/
+    List<User> allUsers = new ArrayList<User>();
 
     @Test
     public void givenUsers_whenGetUsers_thenReturnJsonArray() throws Exception {
+        allUsers.add(javier);
+        given(mockedUserRepo.findAll()).willReturn(allUsers);
+
         mockMvc.perform(get("/api/users/all"))
-            .andExpect(status().isOk());
+            .andExpect(status().isOk())
+            .andExpect(content().contentType("application/json"))
+            .andExpect(jsonPath("$", hasSize(1)))
+            .andExpect(jsonPath("$[0].username", is(javier.getUsername() )));
     }
 }
