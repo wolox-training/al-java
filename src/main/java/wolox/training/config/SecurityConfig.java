57cae4427e540544c63425fe8ba791e51c6f45da
adlamas <alan12394@hotmail.com>
2020-06-17 18:29:57 -0300

    created CustomAuthenticationProvider

--- src/main/java/wolox/training/config/SecurityConfig.java
+++ src/main/java/wolox/training/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package wolox.training.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,6 +10,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated()
+            .and().httpBasic();
+    }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
--- src/main/java/wolox/training/providers/CustomAuthenticationProvider.java
+++ src/main/java/wolox/training/providers/CustomAuthenticationProvider.java
@@ -1,11 +1,16 @@
 package wolox.training.providers;
 
+import java.util.ArrayList;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
+import wolox.training.models.User;
 import wolox.training.repositories.UserRepository;
+import wolox.training.services.passwordEncoderService;
 
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
@@ -14,13 +19,21 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
     private UserRepository userRepository;
 
     @Override
-    public Authentication authenticate(Authentication authentication)
-        throws AuthenticationException {
-        return null;
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String name = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        Optional<User> user = userRepository.findFirstByUsername(name);
+
+        if (user.isPresent() && passwordEncoderService
+            .matches(password, user.get().getPassword())) {
+            return new UsernamePasswordAuthenticationToken(name, password, new ArrayList<>());
+        } else {
+            return null;
+        }
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
-        return false;
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
     }
 }
