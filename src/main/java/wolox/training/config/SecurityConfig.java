7de3f8fdbe72d23eefb946010e6245a1c9fa01e5
adlamas <alan12394@hotmail.com>
2020-06-17 16:48:18 -0300

    updated WTrainingApplication with exclude Security

---
+++ src/main/java/wolox/training/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package wolox.training.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+            .antMatchers(HttpMethod.POST, "/api/books")
+            .and()
+            .ignoring().antMatchers(HttpMethod.POST, "/api/users");
+    }
+}
